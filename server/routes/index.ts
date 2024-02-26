00c7603ca45ec4293e88ea58e66f7584e49b9b88
hutcheonb <106151419+hutcheonb-moj@users.noreply.github.com>
2024-02-26 14:01:16 +0000

    Add header url to app locals

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -2,15 +2,13 @@ import { type RequestHandler, Router } from 'express'
 
 import asyncMiddleware from '../middleware/asyncMiddleware'
 import type { Services } from '../services'
-import config from '../config'
 
 export default function routes({ supportedPrisonsService }: Services): Router {
   const router = Router()
   const get = (path: string | string[], handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
-  const { oneLoginLink } = config
 
   get('/', (req, res, next) => {
-    res.render('pages/index', { user: req.user, oneLoginLink })
+    res.render('pages/index')
   })
 
   get('/prisons', async (req, res, next) => {
