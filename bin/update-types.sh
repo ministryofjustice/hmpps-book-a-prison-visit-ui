732813aa5d4998fb5eeafee3edd7e9382e63638b
Thomas McGowan <tpmcgowan@users.noreply.github.com>
2024-04-18 13:45:17 +0100

    Add API definitions and import script

---
+++ bin/update-types.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# Download API definitions and update TypeScript files
+# Also automatically tidies and fixes linting issues
+
+APIS='{
+  "apis": [
+    {
+      "api": "booker-registry-api",
+      "url": "https://hmpps-prison-visit-booker-registry-dev.prison.service.justice.gov.uk/v3/api-docs"
+    },
+    {
+      "api": "orchestration-api",
+      "url": "https://hmpps-manage-prison-visits-orchestration-dev.prison.service.justice.gov.uk/v3/api-docs"
+    }
+  ]
+}'
+
+echo $APIS | jq -c '.apis[]' | while read API; do
+  API_NAME=$(jq -r '.api' <<< $API)
+  API_URL=$(jq -r '.url' <<< $API)
+
+  echo "\nProcessing $API_NAME..."
+
+  npx openapi-typescript $API_URL --output "./server/@types/$API_NAME.d.ts"
+
+  echo "..tidying up..."
+  sed -i '' 's/^export interface external {}$/\/\/ eslint-disable-next-line @typescript-eslint\/no-empty-interface\n&/'  "./server/@types/$API_NAME.d.ts"
+  npx eslint  "./server/@types/$API_NAME.d.ts" --fix
+done
+
+echo "\nNow running type check:"
+npm run typecheck
