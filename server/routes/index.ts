1f98053db21764891068a70443b2242aa5bbc362
Thomas McGowan <tpmcgowan@users.noreply.github.com>
2024-04-29 16:28:20 +0100

    Show booker's prisoner on home page

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -3,13 +3,16 @@ import { type RequestHandler, Router } from 'express'
 import asyncMiddleware from '../middleware/asyncMiddleware'
 import type { Services } from '../services'
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+// TODO use controller pattern like in Admin UI?
 export default function routes(service: Services): Router {
   const router = Router()
   const get = (path: string | string[], handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
 
-  get('/', (req, res, next) => {
-    res.render('pages/index')
+  get('/', async (req, res) => {
+    const { bookerReference } = res.locals.user
+    const prisoner = await service.userService.getPrisoner(bookerReference)
+
+    res.render('pages/index', { prisoner })
   })
 
   return router
