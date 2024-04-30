530640d3a354c3c8aa8d5d74d08fa59a8f558b78
Thomas McGowan <tpmcgowan@users.noreply.github.com>
2024-04-30 07:46:15 +0100

    Fix linting

--- server/routes/bookingJourney/index.ts
+++ server/routes/bookingJourney/index.ts
@@ -5,6 +5,7 @@ import type { Services } from '../../services'
 import SelectPrisonerController from './selectPrisonerController'
 import SelectVisitorsController from './selectVisitorsController'
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default function routes(services: Services): Router {
   const router = Router()
 
