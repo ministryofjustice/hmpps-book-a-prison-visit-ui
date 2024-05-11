fbe06ef2af85e8af365eaaf1863cdea2fe265d4d
Thomas McGowan <tpmcgowan@users.noreply.github.com>
2024-05-11 14:48:06 +0100

    Tweak populateCurrenBooker() and add tests

--- server/services/testutils/mocks.ts
+++ server/services/testutils/mocks.ts
@@ -1,3 +1,22 @@
+/* eslint-disable import/first */
+/*
+ * Import from '..' (server/data/index.ts) fails if applicationInfo not mocked first. This is
+ * because paths in it differ between running app (in 'dist') and where ts-jest runs.
+ */
+import type { ApplicationInfo } from '../../applicationInfo'
+
+const testAppInfo: ApplicationInfo = {
+  applicationName: 'test',
+  buildNumber: '1',
+  gitRef: 'long ref',
+  gitShortHash: 'short ref',
+  branchName: 'main',
+}
+
+jest.mock('../../applicationInfo', () => {
+  return jest.fn(() => testAppInfo)
+})
+
 import { BookerService } from '..'
 
 jest.mock('..')
