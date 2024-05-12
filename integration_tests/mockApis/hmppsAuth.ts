b1722aa4701ab02528918e1d06297a566c4f98d8
Thomas McGowan <tpmcgowan@users.noreply.github.com>
2024-05-12 12:26:02 +0100

    Fix and tidy integration log in tests

--- integration_tests/mockApis/hmppsAuth.ts
+++ integration_tests/mockApis/hmppsAuth.ts
@@ -1,20 +1,4 @@
-import jwt from 'jsonwebtoken'
-
 import { stubFor } from './wiremock'
-
-const createToken = () => {
-  const payload = {
-    sub: 'system_client_id',
-    grant_type: 'client_credentials',
-    scope: ['read', 'write'],
-    auth_source: 'none',
-    iss: 'http://localhost:9091/auth/auth/issuer',
-    authorities: ['ROLE_VISIT_SCHEDULER'],
-    jti: 'NBmv9IH_xw89YFE_tFoBwI1zo9Y',
-    client_id: 'system_client_id',
-  }
-  return jwt.sign(payload, 'secret', { expiresIn: '1h' })
-}
 
 const stubHmppsAuthToken = () =>
   stubFor({
@@ -26,13 +10,12 @@ const stubHmppsAuthToken = () =>
       status: 200,
       headers: {
         'Content-Type': 'application/json;charset=UTF-8',
-        Location: 'http://localhost:3007/sign-in/callback?code=codexxxx&state=stateyyyy',
       },
       jsonBody: {
-        access_token: createToken(),
+        access_token: 'hmpps-auth-token',
         token_type: 'bearer',
         expires_in: 599,
-        scope: 'read',
+        scope: 'read write',
         sub: 'system_client_id',
         auth_source: 'none',
         jti: 'NBmv9IH_xw89YFE_tFoBwI1zo9Y',
