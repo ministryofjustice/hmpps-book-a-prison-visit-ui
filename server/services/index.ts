f6c48237d6d0b05e1cba7debe37360ae70ca0e88
Thomas McGowan <tpmcgowan@users.noreply.github.com>
2024-05-12 22:19:05 +0100

    Add prison service

--- server/services/index.ts
+++ server/services/index.ts
@@ -1,17 +1,21 @@
 import { dataAccess } from '../data'
 import BookerService from './bookerService'
+import PrisonService from './prisonService'
 
 export const services = () => {
   const { applicationInfo, hmppsAuthClient, orchestrationApiClientBuilder } = dataAccess()
 
   const bookerService = new BookerService(orchestrationApiClientBuilder, hmppsAuthClient)
 
+  const prisonService = new PrisonService(orchestrationApiClientBuilder, hmppsAuthClient)
+
   return {
     applicationInfo,
     bookerService,
+    prisonService,
   }
 }
 
 export type Services = ReturnType<typeof services>
 
-export { BookerService }
+export { BookerService, PrisonService }
