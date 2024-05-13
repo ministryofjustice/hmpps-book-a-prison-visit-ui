c3d320adb8972ee416e5b8a6032e2e35f42310ea
Thomas McGowan <tpmcgowan@users.noreply.github.com>
2024-05-13 16:54:58 +0100

    Add tests for select prisoner

---
+++ server/routes/bookingJourney/selectPrisonerController.test.ts
@@ -0,0 +1,68 @@
+import type { Express } from 'express'
+import request from 'supertest'
+import * as cheerio from 'cheerio'
+import { SessionData } from 'express-session'
+import { appWithAllRoutes } from '../testutils/appSetup'
+import TestData from '../testutils/testData'
+import { PrisonerInfoDto } from '../../data/orchestrationApiTypes'
+
+let app: Express
+let sessionData: SessionData
+
+afterEach(() => {
+  jest.resetAllMocks()
+})
+
+describe('Select prisoner', () => {
+  const bookerReference = TestData.bookerReference().value
+  const prisoner = TestData.prisonerInfoDto()
+
+  it('should clear any exiting bookingJourney session data, populate new data and redirect to select visitors page', () => {
+    sessionData = {
+      booker: { reference: bookerReference, prisoners: [prisoner] },
+      bookingJourney: { prisoner: { prisonerNumber: 'OLD JOURNEY DATA' } as PrisonerInfoDto },
+    } as SessionData
+
+    app = appWithAllRoutes({ services: {}, sessionData })
+
+    return request(app)
+      .post('/book-a-visit/select-prisoner')
+      .send({ prisonerNumber: prisoner.prisonerNumber })
+      .expect(302)
+      .expect('location', '/book-a-visit/select-visitors')
+      .expect(() => {
+        expect(sessionData).toStrictEqual({
+          booker: {
+            reference: bookerReference,
+            prisoners: [prisoner],
+          },
+          bookingJourney: {
+            prisoner,
+          },
+        } as SessionData)
+      })
+  })
+
+  it('should reject an invalid prisoner number', () => {
+    sessionData = {
+      booker: { reference: bookerReference, prisoners: [prisoner] },
+    } as SessionData
+
+    app = appWithAllRoutes({ services: {}, sessionData })
+
+    return request(app)
+      .post('/book-a-visit/select-prisoner')
+      .send({ prisonerNumber: 'INVALID' })
+      .expect(404)
+      .expect(res => {
+        const $ = cheerio.load(res.text)
+        expect($('h1').text()).toBe('Prisoner not found')
+        expect(sessionData).toStrictEqual({
+          booker: {
+            reference: bookerReference,
+            prisoners: [prisoner],
+          },
+        } as SessionData)
+      })
+  })
+})
