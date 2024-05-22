82e6426a4bab7972dd35a66fd4cfc14523b591a8
Jon Brighton <brightonsbox@hotmail.com>
2024-05-22 17:26:24 +0100

    Remove prometheus metrics

--- server/app.ts
+++ server/app.ts
@@ -4,7 +4,6 @@ import createError from 'http-errors'
 
 import nunjucksSetup from './utils/nunjucksSetup'
 import errorHandler from './errorHandler'
-import { metricsMiddleware } from './monitoring/metricsApp'
 
 import setupGovukOneLogin from './middleware/setUpGovukOneLogin'
 import govukOneLogin from './authentication/govukOneLogin'
@@ -29,7 +28,6 @@ export default function createApp(services: Services): express.Application {
   app.set('trust proxy', true)
   app.set('port', process.env.PORT || 3000)
 
-  app.use(metricsMiddleware)
   app.use(setUpHealthChecks(services.applicationInfo))
   app.use(setUpWebSecurity())
   app.use(setUpWebSession())
