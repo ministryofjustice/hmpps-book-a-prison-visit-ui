437b1d028329a815f391f8b87a1f4e6fa1424890
Thomas McGowan <tpmcgowan@users.noreply.github.com>
2024-06-11 11:51:12 +0100

    Add change/book application service methods

--- server/services/visitService.ts
+++ server/services/visitService.ts
@@ -1,6 +1,6 @@
 import { BookingJourney } from '../@types/bapv'
 import { RestClientBuilder, OrchestrationApiClient, HmppsAuthClient } from '../data'
-import { ApplicationDto } from '../data/orchestrationApiTypes'
+import { ApplicationDto, VisitDto } from '../data/orchestrationApiTypes'
 
 export default class VisitService {
   constructor(
@@ -28,5 +28,40 @@ export default class VisitService {
       visitorIds,
       bookerReference,
     })
+  }
+
+  async changeVisitApplication({ bookingJourney }: { bookingJourney: BookingJourney }): Promise<ApplicationDto> {
+    const token = await this.hmppsAuthClient.getSystemClientToken()
+    const orchestrationApiClient = this.orchestrationApiClientFactory(token)
+
+    const { contact, phoneNumber } = bookingJourney.mainContact
+    const visitContact = {
+      name: typeof contact === 'string' ? contact : `${contact.firstName} ${contact.lastName}`,
+      ...(phoneNumber && { telephone: phoneNumber }),
+    }
+
+    const visitors = bookingJourney.selectedVisitors.map(visitor => {
+      return {
+        nomisPersonId: visitor.visitorId,
+        visitContact: typeof contact === 'object' ? contact.visitorId === visitor.visitorId : false,
+      }
+    })
+
+    return orchestrationApiClient.changeVisitApplication({
+      applicationReference: bookingJourney.applicationReference,
+      applicationRestriction: bookingJourney.sessionRestriction,
+      sessionTemplateReference: bookingJourney.selectedSessionTemplateReference,
+      sessionDate: bookingJourney.selectedSessionDate,
+      visitContact,
+      visitors,
+      visitorSupport: { description: bookingJourney.visitorSupport },
+    })
+  }
+
+  async bookVisit({ applicationReference }: { applicationReference: string }): Promise<VisitDto> {
+    const token = await this.hmppsAuthClient.getSystemClientToken()
+    const orchestrationApiClient = this.orchestrationApiClientFactory(token)
+
+    return orchestrationApiClient.bookVisit({ applicationReference })
   }
 }
