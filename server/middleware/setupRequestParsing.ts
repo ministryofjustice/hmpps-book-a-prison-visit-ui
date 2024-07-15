a09485b2ff43fb686f02165df4ff43f8c5361ea0
Thomas McGowan <tpmcgowan@users.noreply.github.com>
2024-07-15 21:50:25 +0100

    Add cookie page and controller to set cookie

--- server/middleware/setupRequestParsing.ts
+++ server/middleware/setupRequestParsing.ts
@@ -1,8 +1,10 @@
 import express, { Router } from 'express'
+import cookieParser from 'cookie-parser'
 
 export default function setUpWebRequestParsing(): Router {
   const router = express.Router()
   router.use(express.json())
   router.use(express.urlencoded({ extended: true }))
+  router.use(cookieParser())
   return router
 }
--- server/routes/unauthenticatedRoutes.ts
+++ server/routes/unauthenticatedRoutes.ts
@@ -1,14 +1,26 @@
-import { Router } from 'express'
+import { RequestHandler, Router } from 'express'
 
+import { ValidationChain } from 'express-validator'
 import paths from '../constants/paths'
+import CookiesController from './cookiesController'
+import asyncMiddleware from '../middleware/asyncMiddleware'
 
 export default function routes(): Router {
   const router = Router()
+
+  const get = (path: string | string[], handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
+  const postWithValidation = (path: string | string[], validationChain: ValidationChain[], handler: RequestHandler) =>
+    router.post(path, ...validationChain, asyncMiddleware(handler))
 
   // Accessibility statement
   router.get(paths.ACCESSIBILITY, (req, res) => {
     res.render('pages/accessibilityStatement', { showServiceNav: !!req.session.booker })
   })
+
+  // Cookies
+  const cookies = new CookiesController()
+  get(paths.COOKIES, cookies.view())
+  postWithValidation(paths.COOKIES, cookies.validate(), cookies.submit())
 
   // Privacy notice
   router.get(paths.PRIVACY, (req, res) => {
