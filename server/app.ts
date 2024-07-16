196130310cd074821991bcb4e0a18637dd1235e8
Thomas McGowan <tpmcgowan@users.noreply.github.com>
2024-07-16 07:40:56 +0100

    Analytics consent middleware and conditionally load analytics

--- server/app.ts
+++ server/app.ts
@@ -20,6 +20,7 @@ import routes from './routes'
 
 import type { Services } from './services'
 import populateCurrentBooker from './middleware/populateCurrentBooker'
+import analyticsConsent from './middleware/analyticsConsent'
 
 export default function createApp(services: Services): express.Application {
   const app = express()
@@ -36,6 +37,7 @@ export default function createApp(services: Services): express.Application {
   app.use(setUpStaticResources())
   nunjucksSetup(app, services.applicationInfo)
   app.use(setupGovukOneLogin())
+  app.use(analyticsConsent())
   app.use(unauthenticatedRoutes())
   app.use(govukOneLogin.authenticationMiddleware())
   app.use(populateCurrentBooker(services.bookerService))
