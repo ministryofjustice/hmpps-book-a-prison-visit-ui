c99c3dc250dae9d5bd77db48e47e7ea88c385eeb
Thomas McGowan <tpmcgowan@users.noreply.github.com>
2024-07-22 10:00:01 +0100

    Tweak cookie banner JS

--- assets/js/cookieBanner.js
+++ assets/js/cookieBanner.js
@@ -1,51 +1,52 @@
-const cookieBanner = document.getElementById('cookie-banner')
+(() => {
+  function confirmCookiesChoice(acceptAnalytics) {
+    document.getElementById('cookie-banner-main').hidden = true
 
-function confirmCookiesChoice(acceptAnalytics) {
-  document.getElementById('cookie-banner-main').hidden = true
+    const confirmationMessage =
+      acceptAnalytics === 'yes'
+        ? document.getElementById('cookie-banner-accepted')
+        : document.getElementById('cookie-banner-rejected')
 
-  const confirmationMessage =
-    acceptAnalytics === 'yes'
-      ? document.getElementById('cookie-banner-accepted')
-      : document.getElementById('cookie-banner-rejected')
+    confirmationMessage.hidden = false
+    confirmationMessage.tabIndex = -1
+    confirmationMessage.role = 'alert'
+    confirmationMessage.focus()
 
-  confirmationMessage.hidden = false
-  confirmationMessage.tabIndex = -1
-  confirmationMessage.role = 'alert'
-  confirmationMessage.focus()
+    const cookiePageRadio = document.querySelector(`input[name=acceptAnalytics][value=${acceptAnalytics}]`)
+    if (cookiePageRadio) {
+      cookiePageRadio.checked = true
+    }
 
-  const cookiePageRadio = document.querySelector(`input[name=acceptAnalytics][value=${acceptAnalytics}]`)
-  if (cookiePageRadio) {
-    cookiePageRadio.checked = true
+    setAnalyticsPreferenceCookie(acceptAnalytics)
   }
 
-  setAnalyticsPreferenceCookie(acceptAnalytics)
-}
+  function hideCookiesConfirmation(banner) {
+    banner.querySelectorAll('#cookie-banner-accepted, #cookie-banner-rejected')
+      .forEach(message => message.hidden = true)
 
-function hideCookiesConfirmation() {
-  cookieBanner.querySelectorAll('#cookie-banner-accepted, #cookie-banner-rejected')
-    .forEach(message => message.hidden = true)
+      banner.hidden = true
+  }
 
-    cookieBanner.hidden = true
-}
+  function setAnalyticsPreferenceCookie(acceptAnalytics) {
+    const expires = new Date()
+    expires.setFullYear(expires.getFullYear() + 1)
+    const cookieValue = JSON.stringify({ acceptAnalytics })
+    const cookie = `cookie_policy=${cookieValue}; expires=${expires.toUTCString()}; path=/; Secure`
 
-function setAnalyticsPreferenceCookie(acceptAnalytics) {
-  const expires = new Date()
-  expires.setFullYear(expires.getFullYear() + 1)
-  const cookieValue = JSON.stringify({ acceptAnalytics })
-  const cookie = `cookie_policy=${cookieValue}; expires=${expires.toUTCString()}; path=/; Secure`
+    document.cookie = cookie
+  }
 
-  document.cookie = cookie
-}
+  const cookieBanner = document.getElementById('cookie-banner')
+  if (cookieBanner) {
+    cookieBanner.hidden = false
 
-if (cookieBanner) {
-  cookieBanner.hidden = false
-
-  cookieBanner.querySelectorAll('#cookie-banner-main button').forEach(button => {
-    button.addEventListener('click', () => confirmCookiesChoice(button.value))
-  })
-
-  cookieBanner.querySelectorAll('#cookie-banner-accepted button, #cookie-banner-rejected button')
-    .forEach(button => {
-      button.addEventListener('click', () => hideCookiesConfirmation())
+    cookieBanner.querySelectorAll('#cookie-banner-main button').forEach(button => {
+      button.addEventListener('click', () => confirmCookiesChoice(button.value))
     })
-}
+
+    cookieBanner.querySelectorAll('#cookie-banner-accepted button, #cookie-banner-rejected button')
+      .forEach(button => {
+        button.addEventListener('click', () => hideCookiesConfirmation(cookieBanner))
+      })
+  }
+})()
