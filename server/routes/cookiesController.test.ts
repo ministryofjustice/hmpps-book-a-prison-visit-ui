aa9d7ca272f1490582f6a910619287884cf8f1bc
Thomas McGowan <tpmcgowan@users.noreply.github.com>
2024-07-29 17:55:10 +0100

    VB-4264 Remove analytics cookies when declining cookie consent (#142)

--- server/routes/cookiesController.test.ts
+++ server/routes/cookiesController.test.ts
@@ -128,13 +128,35 @@ describe('Cookies page', () => {
         .expect('Set-Cookie', `cookie_policy={"acceptAnalytics":"yes"}; Path=/; Expires=${expectedCookieExpiry}`)
     })
 
-    it('should set cookie with correct parameters when analytics rejected', () => {
+    it('should set cookie with correct parameters when analytics rejected and clear existing analytics cookies (localhost)', () => {
+      const acceptAnalyticsNoCookie = `cookie_policy={"acceptAnalytics":"no"}; Path=/; Expires=${expectedCookieExpiry}`
+      const clearGACookie = '_ga=; Domain=localhost; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT'
+      const clearGAIDCookie = '_ga_SSLMWLQYHQ=; Domain=localhost; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT'
+
       return request(app)
         .post(paths.COOKIES)
         .send({ acceptAnalytics: 'no' })
         .expect(302)
         .expect('Location', paths.COOKIES)
-        .expect('Set-Cookie', `cookie_policy={"acceptAnalytics":"no"}; Path=/; Expires=${expectedCookieExpiry}`)
+        .expect('Set-Cookie', `${acceptAnalyticsNoCookie},${clearGACookie},${clearGAIDCookie}`)
+    })
+
+    it('should set cookie with correct parameters when analytics rejected and clear existing analytics cookies (justice domain)', () => {
+      const replacedProp = jest.replaceProperty(config, 'domain', 'https://visit-dev.prison.service.justice.gov.uk')
+
+      const acceptAnalyticsNoCookie = `cookie_policy={"acceptAnalytics":"no"}; Path=/; Expires=${expectedCookieExpiry}`
+      const clearGACookie = '_ga=; Domain=justice.gov.uk; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT'
+      const clearGAIDCookie = '_ga_SSLMWLQYHQ=; Domain=justice.gov.uk; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT'
+
+      return request(app)
+        .post(paths.COOKIES)
+        .send({ acceptAnalytics: 'no' })
+        .expect(302)
+        .expect('Location', paths.COOKIES)
+        .expect('Set-Cookie', `${acceptAnalyticsNoCookie},${clearGACookie},${clearGAIDCookie}`)
+        .expect(() => {
+          replacedProp.replaceValue('domain')
+        })
     })
 
     describe('Validation errors', () => {
