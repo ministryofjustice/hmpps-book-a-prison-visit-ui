98a4b8937d4ed95eeda21f45961b07cb2da2e96b
hutcheonb <106151419+hutcheonb-moj@users.noreply.github.com>
2024-08-09 09:08:53 +0100

    Remove unused session variable

--- server/@types/bapv.d.ts
+++ server/@types/bapv.d.ts
@@ -5,7 +5,6 @@ import { Prisoner, Visitor } from '../services/bookerService'
 export type Booker = {
   reference: string
   prisoners?: Prisoner[] // prisoners this booker can book for
-  allVisitors?: Visitor[]
 }
 
 // data that is built up during a booking journey
