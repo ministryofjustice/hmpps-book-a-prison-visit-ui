f0e6f112facfed195d927ae7ce07604a4494c745
Thomas McGowan <tpmcgowan@users.noreply.github.com>
2024-09-04 11:23:45 +0100

    Refactor to use clearSession elsewhere

--- server/routes/bookVisit/cannotBookController.ts
+++ server/routes/bookVisit/cannotBookController.ts
@@ -1,4 +1,5 @@
 import type { RequestHandler } from 'express'
+import { clearSession } from '../../utils/utils'
 
 export default class CannotBookController {
   public constructor() {}
@@ -6,7 +7,7 @@ export default class CannotBookController {
   public view(): RequestHandler {
     return async (req, res) => {
       const { prisoner } = req.session.bookingJourney
-      delete req.session.bookingJourney
+      clearSession(req)
 
       return res.render('pages/bookVisit/cannotBook', { prisoner })
     }
