ce82d22492b9ae7318b2509f8a8d820a41073c21
hutcheonb-moj <106151419+hutcheonb-moj@users.noreply.github.com>
2024-10-24 10:59:42 +0100

    VB-4471, booking confirmation page content changes (#201)

--- integration_tests/pages/bookVisit/visitBooked.ts
+++ integration_tests/pages/bookVisit/visitBooked.ts
@@ -5,7 +5,7 @@ export default class VisitBookedPage extends Page {
     super('Visit booked')
   }
 
-  bookingReference = (): PageElement => cy.get('[data-test="booking-reference"]')
+  bookingReference = (): PageElement => cy.get('[data-test="booking-reference-title"]')
 
   phoneNumberText = (): PageElement => cy.get('[data-test="phone-number-text"]')
 }
