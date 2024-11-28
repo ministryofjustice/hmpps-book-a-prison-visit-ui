b1327cec24bdd3d678d534a6155492fc67580ad7
Thomas McGowan <tpmcgowan@users.noreply.github.com>
2024-11-28 20:02:17 +0000

    Add back link to Check details page

--- server/routes/bookVisit/checkVisitDetailsController.test.ts
+++ server/routes/bookVisit/checkVisitDetailsController.test.ts
@@ -78,7 +78,7 @@ describe('Check visit details', () => {
           const $ = cheerio.load(res.text)
           expect($('title').text()).toMatch(/^Check the visit details before booking -/)
           expect($('#service-header__nav').length).toBe(0)
-          expect($('[data-test="back-link"]').length).toBe(0)
+          expect($('[data-test="back-link"]').attr('href')).toBe(paths.BOOK_VISIT.CONTACT_DETAILS)
           expect($('h1').text()).toBe('Check the visit details before booking')
           expect($('[data-test="prisoner-name"]').text()).toBe('John Smith')
           expect($('[data-test="visitor-name-1"]').text()).toBe('Joan Phillips (44 years old)')
@@ -107,7 +107,7 @@ describe('Check visit details', () => {
         .expect(res => {
           const $ = cheerio.load(res.text)
           expect($('title').text()).toMatch(/^Check the visit details before booking -/)
-          expect($('[data-test="back-link"]').length).toBe(0)
+          expect($('[data-test="back-link"]').attr('href')).toBe(paths.BOOK_VISIT.CONTACT_DETAILS)
           expect($('h1').text()).toBe('Check the visit details before booking')
           expect($('[data-test="prisoner-name"]').text()).toBe('John Smith')
           expect($('[data-test="additional-support"]').text()).toBe('None')
