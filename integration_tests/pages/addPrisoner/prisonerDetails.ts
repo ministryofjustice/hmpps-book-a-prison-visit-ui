dd9586d43ee15fc7e189a07d56ffce55d449a7cf
Thomas McGowan <tpmcgowan@users.noreply.github.com>
2025-04-23 08:40:38 +0100

    VB-5388 Add prisoner integration tests (#301)

---
+++ integration_tests/pages/addPrisoner/prisonerDetails.ts
@@ -0,0 +1,30 @@
+import Page from '../page'
+
+export default class PrisonerDetailsPage extends Page {
+  constructor() {
+    super('Prisoner details')
+  }
+
+  enterFirstName = (firstName: string): void => {
+    cy.get('#firstName').type(firstName)
+  }
+
+  enterLastName = (lastName: string): void => {
+    cy.get('#lastName').clear()
+    cy.get('#lastName').type(lastName)
+  }
+
+  enterPrisonerDob = (day: number, month: number, year: number): void => {
+    cy.get('#prisonerDob-day').type(day.toString())
+    cy.get('#prisonerDob-month').type(month.toString())
+    cy.get('#prisonerDob-year').type(year.toString())
+  }
+
+  enterPrisonNumber = (prisonNumber: string): void => {
+    cy.get('#prisonNumber').type(prisonNumber)
+  }
+
+  continue = (): void => {
+    cy.get('[data-test="continue-button"]').click()
+  }
+}
