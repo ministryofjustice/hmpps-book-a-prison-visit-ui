67721509425a0a2f0f349033772ad18d2d6b7217
Simon Nebesnuick <118196897+simonnebMOJ@users.noreply.github.com>
2025-05-02 11:25:57 +0100

    linter

--- server/routes/visitors/visitorsController.test.ts
+++ server/routes/visitors/visitorsController.test.ts
@@ -75,7 +75,7 @@ describe('Visitors page', () => {
 
         expect($('[data-test="prisoner-name"]').text()).toBe('John Smith')
         expect($('[data-test="visitor-name-1"]').length).toBe(0)
-        expect($('[data-test=no-visitors]').text().trim()).toBe('Warning\nNo visitors are currently approved.');
+        expect($('[data-test=no-visitors]').text().trim()).toBe('Warning\nNo visitors are currently approved.')
 
         expect(bookerService.getVisitors).toHaveBeenCalledWith(bookerReference, prisoner.prisonerNumber)
       })
