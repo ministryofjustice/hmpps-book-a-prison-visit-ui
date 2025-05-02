a60b9969d2f80206073a653deb42dbfcfa92a474
Simon Nebesnuick <118196897+simonnebMOJ@users.noreply.github.com>
2025-05-02 10:54:21 +0100

    unit test text update

--- server/routes/visitors/visitorsController.test.ts
+++ server/routes/visitors/visitorsController.test.ts
@@ -75,7 +75,7 @@ describe('Visitors page', () => {
 
         expect($('[data-test="prisoner-name"]').text()).toBe('John Smith')
         expect($('[data-test="visitor-name-1"]').length).toBe(0)
-        expect($('[data-test=no-visitors]').text()).toBe('No visitor details found.')
+        expect($('[data-test=no-visitors]').text()).toBe('No visitors are currently approved.')
 
         expect(bookerService.getVisitors).toHaveBeenCalledWith(bookerReference, prisoner.prisonerNumber)
       })
