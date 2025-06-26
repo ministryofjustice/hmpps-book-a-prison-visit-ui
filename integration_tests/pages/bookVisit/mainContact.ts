959eab3c14f1b155aaa491a3d31d9f1003df1d30
hutcheonb-moj <106151419+hutcheonb-moj@users.noreply.github.com>
2025-06-26 15:37:43 +0100

    VB-5079 Accessibility improvements (#333)
    Co-authored-by: Thomas McGowan <tpmcgowan@users.noreply.github.com>

--- integration_tests/pages/bookVisit/mainContact.ts
+++ integration_tests/pages/bookVisit/mainContact.ts
@@ -2,11 +2,7 @@ import Page from '../page'
 
 export default class MainContactPage extends Page {
   constructor() {
-    super('Who is the main contact for this booking?', {
-      // Known issue with radio conditional reveal. See:
-      // https://github.com/alphagov/govuk-frontend/issues/979
-      axeRulesToIgnore: ['aria-allowed-attr'],
-    })
+    super('Who is the main contact for this booking?')
   }
 
   selectVisitorByName = (name: string): void => {
