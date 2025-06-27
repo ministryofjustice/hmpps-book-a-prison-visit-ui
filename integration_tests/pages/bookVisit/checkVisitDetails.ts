9ccc9346c230db451269dc917c023d279444aff7
Thomas McGowan <tpmcgowan@users.noreply.github.com>
2025-06-27 09:24:08 +0100

    Check visit details page - minor changes

--- integration_tests/pages/bookVisit/checkVisitDetails.ts
+++ integration_tests/pages/bookVisit/checkVisitDetails.ts
@@ -2,10 +2,12 @@ import Page, { PageElement } from '../page'
 
 export default class CheckVisitDetailsPage extends Page {
   constructor() {
-    super('Check the visit details before booking')
+    super('Check the visit details')
   }
 
   prisonerName = (): PageElement => cy.get('[data-test="prisoner-name"]')
+
+  prisonName = (): PageElement => cy.get('[data-test="prison-name"]')
 
   visitorName = (index: number): PageElement => cy.get(`[data-test="visitor-name-${index}"]`)
 
@@ -21,5 +23,5 @@ export default class CheckVisitDetailsPage extends Page {
 
   contactDetailsPhone = (): PageElement => cy.get('[data-test="contact-details-phone"]')
 
-  continue = (): void => this.clickDisabledOnSubmitButton('submit-booking')
+  submit = (): void => this.clickDisabledOnSubmitButton('submit')
 }
