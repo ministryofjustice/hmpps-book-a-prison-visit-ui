c9cfd6f4469eec548bd6d14cef3bf0b22c31ac95
Thomas McGowan <tpmcgowan@users.noreply.github.com>
2025-07-02 11:14:29 +0100

    Re-use Redis client in token stores

--- server/data/tokenStore/tokenStoreFactory.ts
+++ server/data/tokenStore/tokenStoreFactory.ts
@@ -1,12 +1,17 @@
 import config from '../../config'
-import { createRedisClient } from '../redisClient'
+import { createRedisClient, RedisClient } from '../redisClient'
 import InMemoryTokenStore from './inMemoryTokenStore'
 import RedisTokenStore from './redisTokenStore'
 import { TokenStore, TokenStorePrefix } from './tokenStore'
 
+let redisClient: RedisClient
+
 const tokenStoreFactory = (prefix: TokenStorePrefix): TokenStore => {
   if (config.redis.enabled) {
-    return new RedisTokenStore(createRedisClient(), prefix)
+    if (!redisClient) {
+      redisClient = createRedisClient()
+    }
+    return new RedisTokenStore(redisClient, prefix)
   }
 
   return new InMemoryTokenStore()
