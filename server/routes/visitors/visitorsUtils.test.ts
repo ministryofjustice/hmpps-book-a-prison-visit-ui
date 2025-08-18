9a033563e4443497d118b82831455cd34b187f40
hutcheonb <106151419+hutcheonb-moj@users.noreply.github.com>
2025-08-18 11:30:43 +0100

    Remove unnecessary tests

--- server/routes/visitors/visitorsUtils.test.ts
+++ server/routes/visitors/visitorsUtils.test.ts
@@ -64,34 +64,9 @@ describe('getVisitorAvailabilityDescription', () => {
     [
       'Banned visitors, with expiry',
       [
-        { restrictionType: 'BAN', expiryDate: '2029-01-01' }, // 2029
         { restrictionType: 'BAN', expiryDate: '2030-01-01' }, // 2030
       ] as Visitor['visitorRestrictions'],
       { text: 'Banned until 1 January 2030', class: 'warning' },
-    ],
-    [
-      'Banned visitors (duplicate, reversed order), with expiry',
-      [
-        { restrictionType: 'BAN', expiryDate: '2030-01-01' }, // 2030
-        { restrictionType: 'BAN', expiryDate: '2025-01-01' }, // 2029
-      ] as Visitor['visitorRestrictions'],
-      { text: 'Banned until 1 January 2030', class: 'warning' },
-    ],
-    [
-      'Banned visitors, one with expiry, one without',
-      [
-        { restrictionType: 'BAN', expiryDate: '2029-01-01' }, // 2029
-        { restrictionType: 'BAN', expiryDate: null }, // permanent
-      ] as Visitor['visitorRestrictions'],
-      { text: 'Banned', class: 'warning' },
-    ],
-    [
-      'Banned visitors, one with expiry, one without (duplicate, reversed order)',
-      [
-        { restrictionType: 'BAN', expiryDate: null }, // permanent
-        { restrictionType: 'BAN', expiryDate: '2029-01-01' }, // 2029
-      ] as Visitor['visitorRestrictions'],
-      { text: 'Banned', class: 'warning' },
     ],
   ])(
     '%s - %s - %s',
