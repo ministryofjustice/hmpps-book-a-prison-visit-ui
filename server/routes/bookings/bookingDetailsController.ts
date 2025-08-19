71c98eb5cd2d19ad93ce7063ad0e9d767d8de6cb
hutcheonb <106151419+hutcheonb-moj@users.noreply.github.com>
2025-08-19 15:01:11 +0100

    move message logic

--- server/routes/bookings/bookingDetailsController.ts
+++ server/routes/bookings/bookingDetailsController.ts
@@ -24,7 +24,7 @@ export default class BookingDetailsController {
 
       const prison = await this.prisonService.getPrison(visit.prisonId)
 
-      const messages = getVisitMessages(visit)
+      const messages = getVisitMessages(visit, prison.prisonName)
 
       const nowTimestamp = new Date()
       const visitStartTimestamp = new Date(visit.startTimestamp)
--- server/routes/bookings/bookingsUtils.ts
+++ server/routes/bookings/bookingsUtils.ts
@@ -2,34 +2,51 @@ import { MoJAlert } from '../../@types/bapv'
 import { VisitDetails } from '../../services/visitService'
 
 // eslint-disable-next-line import/prefer-default-export
-export const getVisitMessages = (visit: VisitDetails): MoJAlert[] => {
-  if (visit.visitStatus !== 'CANCELLED') {
+export const getVisitMessages = (visit: VisitDetails, prisonName: string): MoJAlert[] => {
+  if (
+    // message only possible if visit is cancelled, or requested/rejected/auto_rejected
+    visit.visitStatus !== 'CANCELLED' &&
+    visit.visitSubStatus !== 'REQUESTED' &&
+    visit.visitSubStatus !== 'REJECTED' &&
+    visit.visitSubStatus !== 'AUTO_REJECTED'
+  ) {
     return []
   }
 
   const message: MoJAlert = {
     variant: 'information',
-    title: 'Visit cancelled',
     showTitleAsHeading: true,
     text: '',
+    title: undefined,
   }
 
-  switch (visit.outcomeStatus) {
-    case 'BOOKER_CANCELLED':
-      message.text = 'You cancelled this visit.'
-      break
+  if (visit.visitSubStatus === 'REQUESTED') {
+    message.title = 'Your request needs to be reviewed'
+    message.text = `This visit is not booked yet. It needs to be checked by ${prisonName}.`
+  } else if (visit.visitSubStatus === 'REJECTED' || visit.visitSubStatus === 'AUTO_REJECTED') {
+    message.text = `This request was rejected by ${prisonName}.`
+  } else {
+    switch (visit.outcomeStatus) {
+      case 'BOOKER_CANCELLED':
+        message.title = 'Visit cancelled'
+        message.text = 'You cancelled this visit.'
+        break
 
-    case 'PRISONER_CANCELLED':
-      message.text = 'This visit was cancelled by the prisoner.'
-      break
+      case 'PRISONER_CANCELLED':
+        message.title = 'Visit cancelled'
+        message.text = 'This visit was cancelled by the prisoner.'
+        break
 
-    case 'VISITOR_CANCELLED':
-      message.text = 'This visit was cancelled by a visitor.'
-      break
+      case 'VISITOR_CANCELLED':
+        message.title = 'Visit cancelled'
+        message.text = 'This visit was cancelled by a visitor.'
+        break
 
-    default:
-      message.text = 'This visit was cancelled by the prison.'
-      break
+      default:
+        message.title = 'Visit cancelled'
+        message.text = 'This visit was cancelled by the prison.'
+        break
+    }
   }
 
   return [message]
