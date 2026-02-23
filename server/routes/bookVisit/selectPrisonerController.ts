ab86593f7a89f08d69a81a8745ef8b36d32f6f5e
Thomas McGowan <tpmcgowan@users.noreply.github.com>
2026-02-23 11:14:14 +0000

    Small refactor

--- server/routes/bookVisit/selectPrisonerController.ts
+++ server/routes/bookVisit/selectPrisonerController.ts
@@ -3,6 +3,7 @@ import { NotFound } from 'http-errors'
 import paths from '../../constants/paths'
 import { clearSession } from '../../utils/utils'
 import { BookerService, PrisonService } from '../../services'
+import { BookVisitJourney } from '../../@types/bapv'
 
 export default class SelectPrisonerController {
   public constructor(
@@ -16,21 +17,19 @@ export default class SelectPrisonerController {
 
       const { booker } = req.session
       const prisoner = booker.prisoners[0]
+      const bookVisitJourney: BookVisitJourney = { prisoner }
 
       if (req.body?.prisonerDisplayId !== prisoner.prisonerDisplayId) {
         throw new NotFound('Prisoner not found')
       }
 
-      const validationResult = await this.bookerService.validatePrisoner(booker.reference, prisoner.prisonerNumber)
-      if (validationResult === true) {
-        const prison = await this.prisonService.getPrison(prisoner.prisonId)
-        req.session.bookVisitJourney = { prisoner, prison }
-      } else {
-        req.session.bookVisitJourney = { prisoner }
-      }
+      req.session.bookVisitJourney = bookVisitJourney
 
+      const validationResult = await this.bookerService.validatePrisoner(booker.reference, prisoner.prisonerNumber)
       const prisonerHasVOsOrRemand = prisoner.availableVos > 0 || prisoner.convictedStatus === 'Remand'
+
       if (validationResult === true && prisonerHasVOsOrRemand) {
+        bookVisitJourney.prison = await this.prisonService.getPrison(prisoner.prisonId)
         return res.redirect(paths.BOOK_VISIT.SELECT_VISITORS)
       }
 
@@ -39,15 +38,15 @@ export default class SelectPrisonerController {
         validationResult === 'PRISONER_TRANSFERRED_SUPPORTED_PRISON' ||
         validationResult === 'PRISONER_TRANSFERRED_UNSUPPORTED_PRISON'
       ) {
-        req.session.bookVisitJourney.cannotBookReason = 'TRANSFER_OR_RELEASE'
+        bookVisitJourney.cannotBookReason = 'TRANSFER_OR_RELEASE'
       }
 
       if (validationResult === 'REGISTERED_PRISON_NOT_SUPPORTED') {
-        req.session.bookVisitJourney.cannotBookReason = 'UNSUPPORTED_PRISON'
+        bookVisitJourney.cannotBookReason = 'UNSUPPORTED_PRISON'
       }
 
       if (validationResult === true) {
-        req.session.bookVisitJourney.cannotBookReason = 'NO_VO_BALANCE'
+        bookVisitJourney.cannotBookReason = 'NO_VO_BALANCE'
       }
 
       return res.redirect(paths.BOOK_VISIT.CANNOT_BOOK)
