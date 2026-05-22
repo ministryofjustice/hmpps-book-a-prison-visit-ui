edce4d7e0bb3474fc959dced50eca398dde48f47
Thomas McGowan <tpmcgowan@users.noreply.github.com>
2026-05-22 08:24:04 +0100

    Simplify setCurrentUrl middleware

--- server/middleware/setCurrentUrl.ts
+++ server/middleware/setCurrentUrl.ts
@@ -1,8 +1,9 @@
 import { RequestHandler } from 'express'
+import config from '../config'
 
 export default function setCurrentUrl(): RequestHandler {
   return (req, res, next) => {
-    const currentUrl = new URL(req.originalUrl, `${req.protocol}://${req.get('host')}`)
+    const currentUrl = new URL(req.originalUrl, config.domain)
 
     res.locals.currentUrlEn = new URL(currentUrl)
     res.locals.currentUrlEn.searchParams.set('lng', 'en')
