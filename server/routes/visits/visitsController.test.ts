ea9f70ce80e4992a46092300b3d867fb7b4ff135
Thomas McGowan <tpmcgowan@users.noreply.github.com>
2026-08-27 12:29:25 +0100

    Add prisoner and prison to past and cancelled visit lists

--- server/routes/visits/visitsController.test.ts
+++ server/routes/visits/visitsController.test.ts
@@ -198,6 +198,7 @@ describe('Past visits list page', () => {
         expect($('h1').text()).toBe('Past visits')
 
         expect($('[data-test="visit-date-1"]').text()).toBe('Thursday 30 May 2024')
+        expect($('[data-test="visit-prisoner-name-1"]').text()).toBe('John Smith at Hewell (HMP & YOI)')
         expect($('[data-test="visit-start-end-time-1"]').text()).toBe('10am to 11:30am')
         expect($('[data-test="visit-link-1"]').attr('href')).toBe(
           `${paths.VISITS.VISIT_PAST}/${pastVisitDetails.visitDisplayId}`,
@@ -253,6 +254,7 @@ describe('Cancelled visits list page', () => {
         expect($('h1').text()).toBe('Rejected and cancelled visits')
 
         expect($('[data-test="visit-date-1"]').text()).toBe('Thursday 30 May 2024')
+        expect($('[data-test="visit-prisoner-name-1"]').text()).toBe('John Smith at Hewell (HMP & YOI)')
         expect($('[data-test="visit-start-end-time-1"]').text()).toBe('10am to 11:30am')
         expect($('[data-test="visit-link-1"]').attr('href')).toBe(
           `${paths.VISITS.VISIT_CANCELLED}/${cancelledVisitDetails.visitDisplayId}`,
